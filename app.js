b81f981fbea9725533f108e9a410079046340b43
aygul2018 <teacher.aygunm@gmail.com>
2024-06-19 21:03:37 +0400

    test

---
+++ app.js
@@ -0,0 +1,25 @@
+// let text="JS is a high-level interpreted language.";
+// console.log(text);
+// console.log(text.length);
+// console.log(text.includes("e"));
+// console.log(text.startsWith ("code"));
+// console.log(text.endsWith("code"));
+// console.log(text.replaceAll ("t","#"));
+
+
+
+// let fullname="Jane Browns"
+// console.log(fullname);
+// console.log(fullname.length);
+// let name=prompt("enter your name")
+// console.log(name);
+
+// let number=prompt("enter two numbers");
+// let num=40;
+// console.log(number+num);
+// console.log(number-num);
+// console.log(number*num);
+// console.log(number/num);
+let num=12345;
+let number=98765;
+console.log(num*number);
